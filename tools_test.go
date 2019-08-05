2491885e7deb9e5c85d0f173a4811c8e36d928ff
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-08-05 23:26:27 +0300

    added tests

--- tools_test.go
+++ tools_test.go
@@ -6,6 +6,28 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestIsDomain(t *testing.T) {
+	assert.True(t, IsDomain("example.com", false))
+	assert.False(t, IsDomain("example.com", true))
+	assert.True(t, IsDomain("example.com.", true))
+	assert.False(t, IsDomain("", false))
+	assert.True(t, IsDomain("x", false))
+}
+
+func TestInZone(t *testing.T) {
+	assert.True(t, InZone("example.com.", "foo.example.com."))
+	assert.True(t, InZone("example.com", "foo.example.com"))
+	assert.True(t, InZone("example.com", "example.com"))
+	assert.False(t, InZone("foo.example.com", "example.com"))
+}
+
+func TestTrimZone(t *testing.T) {
+	assert.Equal(t, "foo", TrimZone("example.com.", "foo.example.com."))
+	assert.Equal(t, "foo", TrimZone("example.com", "foo.example.com"))
+	assert.Equal(t, "", TrimZone("example.com", "example.com"))
+	assert.Equal(t, "example.com", TrimZone("foo.example.com", "example.com"))
+}
+
 func TestTransferCase(t *testing.T) {
 	table := []struct {
 		src string
